b0efd3a2452b0d5b1f87d9f1a86a6872023f297b
daoran <lry20Autumn@126.com>
2024-10-17 10:30:44 +0800

    zhangyan

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/controller/SciProjectScoreCfgController.java
@@ -0,0 +1,162 @@
+package com.ruoyi.system.controller;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.ruoyi.common.utils.ShiroUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.system.domain.SciProjectScoreCfg;
+import com.ruoyi.system.service.ISciProjectScoreCfgService;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.common.core.page.TableDataInfo;
+
+/**
+ * 横向课题得分配置Controller
+ *
+ * @author ruoyi
+ * @date 2024-09-30
+ */
+@Controller
+@RequestMapping("/system/projectScoreCfg")
+public class SciProjectScoreCfgController extends BaseController {
+    private String prefix = "system/projectScoreCfg";
+
+    @Autowired
+    private ISciProjectScoreCfgService sciProjectScoreCfgService;
+
+    @RequiresPermissions("system:projectScoreCfg:view")
+    @GetMapping()
+    public String projectScoreCfg() {
+        return prefix + "/projectScoreCfg";
+    }
+
+
+    /**
+     * 查询横向课题得分配置列表
+     */
+    @PostMapping("/getCfgCard")
+    @ResponseBody
+    public Map<String, Object> getCfgCard(SciProjectScoreCfg sciProjectScoreCfg) {
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("code", "0");
+        resultMap.put("msg", "操作成功");
+        Map<String, Object> cfgMap = sciProjectScoreCfgService.getProjectScoreCfg();
+        resultMap.put("data", cfgMap);
+        System.out.println("resultMap = " + resultMap);
+        return resultMap;
+    }
+
+    /**
+     * 查询横向课题得分配置列表
+     */
+    @RequiresPermissions("system:projectScoreCfg:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(SciProjectScoreCfg sciProjectScoreCfg) {
+        startPage();
+        List<SciProjectScoreCfg> list = sciProjectScoreCfgService.selectSciProjectScoreCfgList(sciProjectScoreCfg);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出横向课题得分配置列表
+     */
+    @RequiresPermissions("system:projectScoreCfg:export")
+    @Log(title = "横向课题得分配置", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(SciProjectScoreCfg sciProjectScoreCfg) {
+        List<SciProjectScoreCfg> list = sciProjectScoreCfgService.selectSciProjectScoreCfgList(sciProjectScoreCfg);
+        ExcelUtil<SciProjectScoreCfg> util = new ExcelUtil<SciProjectScoreCfg>(SciProjectScoreCfg.class);
+        return util.exportExcel(list, "横向课题得分配置数据");
+    }
+
+    /**
+     * 新增横向课题得分配置
+     */
+    @GetMapping("/add")
+    public String add() {
+        return prefix + "/add";
+    }
+
+    /**
+     * 新增保存横向课题得分配置
+     */
+    @RequiresPermissions("system:projectScoreCfg:add")
+    @Log(title = "横向课题得分配置", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(@RequestParam("userOrder") String userOrderStr
+            , @RequestParam("totalScore") String totalStoreStr
+            , @RequestParam("startScore") String startScoreStr
+            , @RequestParam("endScore") String endScoreStr
+            , @RequestParam String fundsMax
+            , @RequestParam String fundsMin
+            , @RequestParam String projectType
+    ) {
+        String[] userOrder = userOrderStr.split(",");
+        String[] totalScore = totalStoreStr.split(",");
+        String[] startScore = startScoreStr.split(",");
+        String[] endScore = endScoreStr.split(",");
+//        String fundsMax = map.get("fundsMax") + "";
+//        String fundsMin = map.get("fundsMin") + "";
+//        String projectType = map.get("projectType") + "";
+        for (int i = 0; i < userOrder.length; i++) {
+            SciProjectScoreCfg sciProjectScoreCfg = new SciProjectScoreCfg();
+            sciProjectScoreCfg.setProjectType(projectType);
+            sciProjectScoreCfg.setFundsMax(fundsMax);
+            sciProjectScoreCfg.setFundsMin(fundsMin);
+            sciProjectScoreCfg.setUserOrder(userOrder[i] + "");
+            sciProjectScoreCfg.setTotalScore(totalScore[i] + "");
+            sciProjectScoreCfg.setStartScore(startScore[i] + "");
+            sciProjectScoreCfg.setEndScore(endScore[i] + "");
+            sciProjectScoreCfg.setUpdateUser(ShiroUtils.getUserId() + "");
+            sciProjectScoreCfgService.insertSciProjectScoreCfg(sciProjectScoreCfg);
+        }
+
+        return toAjax(1);
+    }
+
+    /**
+     * 修改横向课题得分配置
+     */
+    @RequiresPermissions("system:projectScoreCfg:edit")
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, ModelMap mmap) {
+        SciProjectScoreCfg sciProjectScoreCfg = sciProjectScoreCfgService.selectSciProjectScoreCfgById(id);
+        mmap.put("sciProjectScoreCfg", sciProjectScoreCfg);
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存横向课题得分配置
+     */
+    @RequiresPermissions("system:projectScoreCfg:edit")
+    @Log(title = "横向课题得分配置", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(SciProjectScoreCfg sciProjectScoreCfg) {
+        return toAjax(sciProjectScoreCfgService.updateSciProjectScoreCfg(sciProjectScoreCfg));
+    }
+
+    /**
+     * 删除横向课题得分配置
+     */
+    @RequiresPermissions("system:projectScoreCfg:remove")
+    @Log(title = "横向课题得分配置", businessType = BusinessType.DELETE)
+    @PostMapping("/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids) {
+        return toAjax(sciProjectScoreCfgService.deleteSciProjectScoreCfgByIds(ids));
+    }
+}
---
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/SciProjectScoreCfgMapper.java
@@ -0,0 +1,88 @@
+package com.ruoyi.system.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ruoyi.system.domain.SciProjectScoreCfg;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * 横向课题得分配置Mapper接口
+ *
+ * @author ruoyi
+ * @date 2024-09-30
+ */
+public interface SciProjectScoreCfgMapper {
+    /**
+     * 查询横向课题得分配置
+     *
+     * @param id 横向课题得分配置主键
+     * @return 横向课题得分配置
+     */
+    public SciProjectScoreCfg selectSciProjectScoreCfgById(Long id);
+
+    /**
+     * 查询横向课题得分配置列表
+     *
+     * @param sciProjectScoreCfg 横向课题得分配置
+     * @return 横向课题得分配置集合
+     */
+    public List<SciProjectScoreCfg> selectSciProjectScoreCfgList(SciProjectScoreCfg sciProjectScoreCfg);
+
+    /**
+     * 新增横向课题得分配置
+     *
+     * @param sciProjectScoreCfg 横向课题得分配置
+     * @return 结果
+     */
+    public int insertSciProjectScoreCfg(SciProjectScoreCfg sciProjectScoreCfg);
+
+    /**
+     * 修改横向课题得分配置
+     *
+     * @param sciProjectScoreCfg 横向课题得分配置
+     * @return 结果
+     */
+    public int updateSciProjectScoreCfg(SciProjectScoreCfg sciProjectScoreCfg);
+
+    /**
+     * 删除横向课题得分配置
+     *
+     * @param id 横向课题得分配置主键
+     * @return 结果
+     */
+    public int deleteSciProjectScoreCfgById(Long id);
+
+    /**
+     * 批量删除横向课题得分配置
+     *
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    public int deleteSciProjectScoreCfgByIds(String[] ids);
+
+    @Select(" SELECT DISTINCT " +
+            " funds_max, " +
+            " funds_min, " +
+            " project_type  " +
+            "FROM " +
+            " sci_project_score_cfg t  " +
+            "ORDER BY " +
+            " FUNDS_MIN + 0 DESC ")
+    List<Map<String, Object>> getCfgFunds();
+
+    @Select(" SELECT " +
+            " t.user_order, " +
+            " t.total_score, " +
+            " t.start_score, " +
+            " t.end_score  " +
+            "FROM " +
+            " sci_project_score_cfg t  " +
+            "WHERE " +
+            " t.funds_max = #{funds_max}  " +
+            " AND t.funds_min = #{funds_min}  " +
+            " AND t.project_type = #{project_type}  " +
+            "ORDER BY " +
+            " t.total_score + 0 DESC ")
+    List<Map<String, Object>> getUserScoreList(Map<String, Object> fundsMap);
+}
