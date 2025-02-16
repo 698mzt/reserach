f4806faf30e4bc99c800a3ac2c046aed9768f1fa
天秦晋 <13300634+qtj1067@user.noreply.gitee.com>
2025-02-16 16:58:42 +0800

    zfl-校内横向流程 修复了申请校内横向课题时第一负责人不固定时登陆人 修复了立项申请添加时应该没有结项日期,申请结项时填写结项日期

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SciIntraSchoolPro.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SciIntraSchoolPro.java
@@ -117,6 +117,27 @@ public class SciIntraSchoolPro extends BaseEntity {
     //判定登陆人的dept是否和当前课题负责人的dept是否相等
     private String deptNamekey;
 
+    //结项合同
+    private String overContract;
+    //结项备案表
+    private String overFiling;
+
+    public String getOverContract() {
+        return overContract;
+    }
+
+    public void setOverContract(String overContract) {
+        this.overContract = overContract;
+    }
+
+    public String getOverFiling() {
+        return overFiling;
+    }
+
+    public void setOverFiling(String overFiling) {
+        this.overFiling = overFiling;
+    }
+
     public String getDeptNamekey() {
         return deptNamekey;
     }
