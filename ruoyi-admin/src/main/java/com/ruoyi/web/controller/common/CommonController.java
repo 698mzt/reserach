4a1051c5dca4399683e01809dfa40b6cda2541bc
14740 <1474024313@qq.com>
2025-03-11 22:57:50 +0800

    文件上传

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CommonController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CommonController.java
@@ -91,17 +91,17 @@ public class CommonController extends BaseController
     /**
      * 通用上传请求（单个）
      */
-    @PostMapping("/upload")
+    @PostMapping("/upload/{model}")
     @ResponseBody
-    public AjaxResult uploadFile(MultipartFile file) throws Exception
+    public AjaxResult uploadFile(MultipartFile file,@PathVariable("model") String model) throws Exception
     {
         try
         {
-
             // 上传文件路径
             String filePath = RuoYiConfig.getUploadPath() ;
             // 上传并返回新文件名称
-            String fileName = FileUploadUtils.upload(filePath, file);
+//            String fileName = FileUploadUtils.upload(filePath, file);
+            String fileName = FileUploadUtils.newupload(filePath, file,model);
             String url = serverConfig.getUrl() + fileName;
             AjaxResult ajax = AjaxResult.success();
             ajax.put("url", url);
