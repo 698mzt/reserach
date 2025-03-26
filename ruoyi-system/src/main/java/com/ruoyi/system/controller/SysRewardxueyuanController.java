5a230e98400383e6840b044616b86b21a09c82a4
15204 <1520495536@qq.com>
2025-03-26 18:52:05 +0800

    aaa

--- ruoyi-system/src/main/java/com/ruoyi/system/controller/SysRewardxueyuanController.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/controller/SysRewardxueyuanController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/aaaaaa")
+@RequestMapping("/rewardxueyuan")
 public class SysRewardxueyuanController extends BaseController {
 
     @Autowired
