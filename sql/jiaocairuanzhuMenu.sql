ab62b5cd06324afc8cee2d708268efac7e546844
14740 <1474024313@qq.com>
2025-08-26 15:08:44 +0800

    定时任务

--- sql/jiaocairuanzhuMenu.sql
+++ sql/jiaocairuanzhuMenu.sql
@@ -20,3 +20,7 @@ values('教材软著删除', @parentId, '4',  '#',  'F', '0', 'system:jiaocairua
 
 insert into sys_menu (menu_name, parent_id, order_num, url, menu_type, visible, perms, icon, create_by, create_time, update_by, update_time, remark)
 values('教材软著导出', @parentId, '5',  '#',  'F', '0', 'system:jiaocairuanzhu:export',       '#', 'admin', sysdate(), '', null, '');
+
+-- 添加数据同步定时任务（每分钟执行一次）
+-- 后续可修改为每天晚上12点执行：0 0 0 * * ?
+INSERT INTO `sys_job` VALUES (4, '数据同步任务', 'SYSTEM', 'dataSynchronizeTask.synchronizeAll()', '0 * * * * ?', '3', '1', '0', 'admin', sysdate(), '', null, '数据同步任务，每分钟执行一次');
