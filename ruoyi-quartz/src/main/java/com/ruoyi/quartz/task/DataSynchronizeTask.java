2510c814d22e566115e35d106931bc6bd17891f4
吴宇飞 <18335094006@139.com>
2025-08-29 15:26:52 +0800

    完善

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/DataSynchronizeTask.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/DataSynchronizeTask.java
@@ -3,6 +3,7 @@ package com.ruoyi.quartz.task;
 import com.ruoyi.system.mapper.SynchronizeDataMapper;
 import com.ruoyi.system.mapper.SynchronizeZlrzMapper;
 import com.ruoyi.system.mapper.SynrewardDataMapper;
+import com.ruoyi.system.mapper.synchronizedFlowMapper;
 import com.ruoyi.system.service.IAlltotleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -26,6 +27,9 @@ public class DataSynchronizeTask {
 
   @Autowired
   private SynrewardDataMapper synrewardDataMapper;
+
+  @Autowired
+  private synchronizedFlowMapper synchronizedFlowMapper;
 
 
   /**
@@ -53,6 +57,10 @@ public class DataSynchronizeTask {
 
     //同步奖励数据
     Reward();
+
+    //同步论文
+    syncronzedFlow();
+
   }
 
 
@@ -114,5 +122,12 @@ public class DataSynchronizeTask {
     System.out.println("奖励数据开始同步");
     synrewardDataMapper.Reward();
     System.out.println("奖励数据同步完成");
+  }
+
+  //同步论文
+  private void syncronzedFlow() {
+    System.out.println("论文数据开始同步");
+    synchronizedFlowMapper.syncronzedFlow();
+    System.out.println("论文数据同步完成");
   }
 }
