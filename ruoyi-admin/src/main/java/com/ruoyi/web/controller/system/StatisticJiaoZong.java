a154d4dc6948bf21b6342196b7611b17017868c6
14740 <1474024313@qq.com>
2025-09-04 14:03:33 +0800

    教研室科研工作量页面

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/StatisticJiaoZong.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/StatisticJiaoZong.java
@@ -10,6 +10,7 @@ import com.ruoyi.framework.web.domain.server.Sys;
 import com.ruoyi.system.mapper.StatisticJZMapper;
 import com.ruoyi.system.mapper.StatisticMapper;
 import com.ruoyi.system.mapper.SysDeptMapper;
+import com.ruoyi.system.service.IStatisticYJCGSService;
 import com.ruoyi.system.service.ISysDeptService;
 import com.ruoyi.system.service.ISysUserService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -24,43 +25,25 @@ import java.util.*;
 @RequestMapping("/statisticJZ")
 public class StatisticJiaoZong extends BaseController {
 
+
     @Autowired
-    private StatisticJZMapper statisticJZMapper;
-    @Autowired
-    private ISysUserService userService;
-    @Autowired
-    private ISysDeptService deptService;
+    private IStatisticYJCGSService statisticYJCGSService;
 
     private String prefix = "system/statistic";
     @GetMapping()
     public String apply()
     {
-        return prefix + "/yjcgJYS.html";
+        return prefix + "/yjcgJYS";
     }
 
     @PostMapping("/listJZ")
     @ResponseBody
     public TableDataInfo listJZ() {
-        startPage();
         String dept = getSysUser().getDeptId().toString();
-//        String ParentId = getSysUser().getParentId().toString();
-//        System.out.println(ParentId);
-
-
-        List<Map<String, Object>> list = statisticJZMapper.selectAllJZ(dept);
-        List<Map<String, Object>> list1 = statisticJZMapper.selectTotalJZ(dept);
-
-        List<SysUser> users = userService.selectAllUser();
-//        todo:获取部门信息错误：list
-
-
-
-
-        list.add(list1.get(0));
+        startPage();
+        List<Map<String, Object>> list = statisticYJCGSService.selectYJCGSJYS(dept);
 
         TableDataInfo data = getDataTable(list);
-        System.out.println(list);
-        System.out.println(list1);
         return data;
     }
 }
--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/StatisticJZMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/StatisticJZMapper.java
@@ -1,11 +1,12 @@
 package com.ruoyi.system.mapper;
 
+import org.apache.ibatis.annotations.MapKey;
+
 import java.util.List;
 import java.util.Map;
 
 public interface StatisticJZMapper {
 
+    @MapKey("userId")
     List<Map<String, Object>> selectAllJZ(String deptId);
-
-    List<Map<String, Object>> selectTotalJZ(String dept);
 }
